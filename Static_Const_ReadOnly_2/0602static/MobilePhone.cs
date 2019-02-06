ee23e4bd3e582f83cdab0c06e2eddeed4d79657e
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-02-06 21:27:49 +0200

    string const read only continue

---
+++ Static_Const_ReadOnly_2/0602static/MobilePhone.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0602static
+{
+    class MobilePhone
+    {
+        public string name;
+        public static int _numberOfInstances;
+        public readonly DateTime creationTime;
+        public const float MAX_PHONE_SIZE = 6.9f;
+        public MobilePhone(string name)
+        {
+            this.name = name;
+            _numberOfInstances++;
+            creationTime = DateTime.Now;
+        }
+
+        public void PrintPhoneName()
+        {
+            Console.WriteLine($"Phone name {this.name}");
+        }
+
+        public static void PrintNumberOfInstances()
+        {
+            Console.WriteLine($"Number of instances : {_numberOfInstances}");
+        }
+
+        public static void PrintMaxPhoneSize()
+        {
+            Console.WriteLine($"Maximum phone size : {MAX_PHONE_SIZE}");
+        }
+
+        public void PrintCreationTime()
+        {
+            Console.WriteLine($"Creation time : {this.creationTime}");
+        }
+
+    }
+}
---
+++ Static_Const_ReadOnly_2/0602static/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _0602static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing please wait 4 seconds...");
+
+            MobilePhone m1 = new MobilePhone("m1");
+            Thread.Sleep(1250);
+            MobilePhone m2 = new MobilePhone("m2");
+            Thread.Sleep(1250);
+            MobilePhone m3 = new MobilePhone("m3");
+            Thread.Sleep(1250);
+
+            MobilePhone.PrintNumberOfInstances();
+
+            m1.PrintCreationTime();
+            m2.PrintCreationTime();
+            m3.PrintCreationTime();
+
+            MobilePhone.PrintMaxPhoneSize();
+        }
+
+
+
+    }
+}
